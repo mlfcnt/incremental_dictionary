da52033ab33e322f2b0fb0a2f4ff4a3d55b4b6f9
TMN <t.martin@adsoftware.fr>
2022-10-02 10:40:59 +0200

    fix: error reset word

--- src/helpers.ts/handleKeyPressed.ts
+++ src/helpers.ts/handleKeyPressed.ts
@@ -9,5 +9,7 @@ export const handleKeyPressed = (
   const letterToGuess = currentWord[currentUserInput?.length || 0];
   if (keyPressed?.toUpperCase() === letterToGuess?.toUpperCase()) {
     setCurrentUserInput(`${currentUserInput || ""}${keyPressed}`);
+  } else {
+    setCurrentUserInput("");
   }
 };
